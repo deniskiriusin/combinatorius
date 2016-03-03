3c053baa222e475f8ad813ccf96d589428659725
denis <deniskir@gmail.com>
2016-03-03 20:32:29 +0000

    Unit test for CSS comments removal regex..

--- src/test/java/com/dkiriusin/combinatorius/RegExTest.java
+++ src/test/java/com/dkiriusin/combinatorius/RegExTest.java
@@ -36,4 +36,34 @@ public class RegExTest {
 		Pattern p = Pattern.compile("(?s)(?i:.*Combinatorius.*)");
 		Assert.assertTrue(p.matcher(text).find());
 	}
+	
+	@Test
+	public void testCSSCommentsRemoval() {
+		final String COMMENT_REGEX = "(/\\*(?!(!))[^*]*\\*+([^/][^*]*\\*+)*/)";
+		StringBuilder sb = new StringBuilder("// comment1");
+		sb.append("\n\n");
+		sb.append("/* comment2 */");
+		sb.append(".className {");
+		sb.append("\n");
+		sb.append("background-color: #fff;");
+		sb.append("\n");
+		sb.append("//width: 100px;");
+		sb.append("\n");
+		sb.append("}");
+		final String text = sb.toString();
+		Pattern COMMENTS_PATTERN = Pattern.compile(COMMENT_REGEX, Pattern.DOTALL);
+		String compressed = COMMENTS_PATTERN.matcher(text).replaceAll("");
+		StringBuilder sb2 = new StringBuilder("// comment1");
+		sb2.append("\n\n");
+		sb2.append(".className {");
+		sb2.append("\n");
+		sb2.append("background-color: #fff;");
+		sb2.append("\n");
+		sb2.append("//width: 100px;");
+		sb2.append("\n");
+		sb2.append("}");
+		Assert.assertTrue(compressed.equalsIgnoreCase(sb2.toString()));
+	}
+	
+	
 }
